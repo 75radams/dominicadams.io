167a3512d9707ff49bc720d70b6c74025f6f38d5
Richard Adams <radams75@gatech.edu>
2021-11-03 09:08:00 -0400

    landing page ux

--- scriptHome.js
+++ scriptHome.js
@@ -1,21 +1,19 @@
-document.getElementById("dominic").onmouseover = function() {
+document.getElementById("about").onmouseover = function() {
     mouseOver1()
 };
 
-document.getElementById("dominic").onmouseout = function() {
+document.getElementById("about").onmouseout = function() {
     mouseOut1()
 };
 
 function mouseOver1() {
-    document.getElementById("dominic").innerHTML = "ABOUT";
+    document.getElementById("about").innerHTML = "ABOUT";
     document.getElementById("dominic").style.left = "20%";
-    document.getElementById("dominic").style.color = "white";
 }
 
 function mouseOut1() {
-    document.getElementById("dominic").innerHTML = "DOMINIC";
+    document.getElementById("about").innerHTML = "DOMINIC";
     document.getElementById("dominic").style.left = "15%";
-    document.getElementById("dominic").style.color = "#00ff7f";
 }
 
 
@@ -23,25 +21,20 @@ function mouseOut1() {
 
 
 
-document.getElementById("adams").onmouseover = function() {
+document.getElementById("project").onmouseover = function() {
     mouseOver2()
 };
 
-document.getElementById("adams").onmouseout = function() {
+document.getElementById("project").onmouseout = function() {
     mouseOut2()
 };
 
 function mouseOver2() {
-    document.getElementById("adams").innerHTML = "PROJECTS";
+    document.getElementById("project").innerHTML = "PROJECTS";
     document.getElementById("adams").style.left = "20%";
-    document.getElementById("adams").style.color = "white";
-    var node = document.createElement("a");
-    node.href = "MyWork.html";
-    document.getElementById("adams").appendChild(node);
 }
 
 function mouseOut2() {
-    document.getElementById("adams").innerHTML = "ADAMS";
+    document.getElementById("project").innerHTML = "ADAMS";
     document.getElementById("adams").style.left = "15%";
-    document.getElementById("adams").style.color = "#00ff7f";
 }
